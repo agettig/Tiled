2a39b57841648c9e2353dacaf1745522f1400679
Ben Neuwirth <bmn36@cornell.edu>
2023-03-25 16:51:44 -0400

    Initial Tiled submodule setup
    - Includes Tiled project, assets, and initial test file

---
+++ objects.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="objects" tilewidth="117" tileheight="129" tilecount="6" columns="0">
+ <editorsettings>
+  <export target="objects.json" format="json"/>
+ </editorsettings>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="file" value="dude"/>
+  </properties>
+  <image width="64" height="113" source="map/bandit.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="file" value="med_robot"/>
+  </properties>
+  <image width="117" height="129" source="map/medium_enemy.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="file" value="small_robot"/>
+  </properties>
+  <image width="43" height="65" source="map/small_enemy.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="file" value="gumProjectile"/>
+  </properties>
+  <image width="25" height="25" source="map/gum.png"/>
+ </tile>
+ <tile id="4" x="0" y="0" width="64" height="64">
+  <properties>
+   <property name="file" value="earth"/>
+  </properties>
+  <image width="268" height="267" source="map/texture_draft.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="file" value="goal"/>
+  </properties>
+  <image width="96" height="96" source="map/goaldoor.png"/>
+ </tile>
+</tileset>
---
+++ readme.txt
@@ -0,0 +1,16 @@
+Using Tiled for this project
+
+To make a new level, you can either copy the existing one and use as a template or create a new map.
+
+Creating a new map: Map size should be Fixed (can be changed later), Width 64px, Height 64px
+A map should have three layers: Objects (object layer), Terrain (tile layer), and Board (tile layer)
+
+Add tiles for the terrain in the Terrain layer.
+
+Add tiles for enemy paths in the Board layer. Blue = floor, Red = ceiling, Green = ladder.
+
+Add player, exit, gum, enemies in Objects layer as an object. Each of them should be a Tile Object. All you have to do is add the object and set the name either "Player", "Gum", "Enemy", or "Exit". For enemies, you also have to set their class to "smallrobot" or "mediumrobot".
+
+To add a new type of object, just go to the objects tileset (objects.tsx) and click add tiles. You will need to change the parsing code in LevelModel to support a new object type.
+
+To add the level to the game, just do Export as JSON and put it in the jsons folder for the game, then change the level json value in assets.json. If you added a new object type, you also have to export the tileset as a json and replace objects.json with it in the jsons folder.
