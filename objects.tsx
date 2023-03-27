9c01e9252bb2e51663efaed8eca871404d8bfaa0
Ben Neuwirth <bmn36@cornell.edu>
2023-03-26 22:53:04 -0400

    Simplify level making process
    - Refactor camera tiles into own tileset
    - No longer need to manually set names and classes for any tiles

--- objects.tsx
+++ objects.tsx
@@ -1,49 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="objects" tilewidth="117" tileheight="129" tilecount="8" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="objects" tilewidth="117" tileheight="129" tilecount="7" columns="0">
  <editorsettings>
-  <export target="objects.json" format="json"/>
+  <export target="../assets/jsons/objects.json" format="json"/>
  </editorsettings>
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <properties>
-   <property name="file" value="dude"/>
-  </properties>
+ <tile id="0" type="bandit">
   <image width="64" height="113" source="map/bandit.png"/>
  </tile>
- <tile id="1">
-  <properties>
-   <property name="file" value="med_robot"/>
-  </properties>
+ <tile id="1" type="mediumrobot">
   <image width="117" height="129" source="map/medium_enemy.png"/>
  </tile>
- <tile id="2">
-  <properties>
-   <property name="file" value="small_robot"/>
-  </properties>
+ <tile id="2" type="smallrobot">
   <image width="43" height="65" source="map/small_enemy.png"/>
  </tile>
- <tile id="3">
-  <properties>
-   <property name="file" value="gumProjectile"/>
-  </properties>
+ <tile id="3" type="floatinggum">
   <image width="25" height="25" source="map/gum.png"/>
  </tile>
- <tile id="4" x="0" y="0" width="64" height="64">
-  <properties>
-   <property name="file" value="earth"/>
-  </properties>
+ <tile id="4" x="0" y="0" width="64" height="64" type="earth">
   <image width="268" height="267" source="map/texture_draft.png"/>
  </tile>
- <tile id="5">
-  <properties>
-   <property name="file" value="goal"/>
-  </properties>
+ <tile id="5" type="exit">
   <image width="96" height="96" source="map/goaldoor.png"/>
  </tile>
- <tile id="6">
-  <image width="64" height="64" source="map/camera_v.png"/>
- </tile>
- <tile id="7">
-  <image width="64" height="64" source="map/camera_h.png"/>
+ <tile id="9" type="orb">
+  <image width="40" height="40" source="map/orb.png"/>
  </tile>
 </tileset>
