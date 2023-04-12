76423dae7ad81010bae13b20c903a5976790ae24
Caroline <crh255@cornell.edu>
2023-04-12 15:14:16 -0400

    support for support tiles in Tiled, haha

---
+++ tilesets/supports.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="supports" tilewidth="64" tileheight="64" tilecount="12" columns="4">
+ <image source="supports.png" trans="ff00ff" width="256" height="193"/>
+ <tile id="0" type="supports"/>
+ <tile id="1" type="supports"/>
+ <tile id="2" type="supports"/>
+ <tile id="3" type="supports"/>
+ <tile id="4" type="supports"/>
+ <tile id="5" type="supports"/>
+ <tile id="6" type="supports"/>
+ <tile id="7" type="supports"/>
+ <tile id="8" type="supports"/>
+ <tile id="9" type="supports"/>
+ <tile id="10" type="supports"/>
+ <tile id="11" type="supports"/>
+</tileset>
