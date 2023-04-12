4668b62f2e220cc3d69a5e7389240d69c93fb167
Ben Neuwirth <bmn36@cornell.edu>
2023-04-12 14:39:19 -0400

    Refactor Tiled folders to make editing easier

--- tilesets/objects.tsx
+++ tilesets/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="objects" tilewidth="134" tileheight="186" tilecount="10" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="objects" tilewidth="144" tileheight="186" tilecount="12" columns="0">
  <editorsettings>
   <export target="../assets/jsons/objects.json" format="json"/>
  </editorsettings>
@@ -20,18 +20,24 @@
   <image width="268" height="267" source="map/texture_draft.png"/>
  </tile>
  <tile id="5" type="exit">
-  <image width="96" height="96" source="map/goaldoor.png"/>
+  <image width="144" height="176" source="map/shipdoor.png"/>
  </tile>
  <tile id="9" type="orb">
   <image width="40" height="40" source="map/orb.png"/>
  </tile>
- <tile id="10" type="Alarm">
+ <tile id="10" type="alarm">
   <image width="44" height="43" source="map/alarm.png"/>
  </tile>
  <tile id="11" type="star">
   <image width="48" height="48" source="map/star.png"/>
  </tile>
- <tile id="12" type="largerobot">
+ <tile id="12" type="large_robot">
   <image width="134" height="186" source="map/large_robot.png"/>
+ </tile>
+ <tile id="13" type="box">
+  <image width="65" height="65" source="map/box.png"/>
+ </tile>
+ <tile id="14" type="chair">
+  <image width="65" height="90" source="map/chair.png"/>
  </tile>
 </tileset>
